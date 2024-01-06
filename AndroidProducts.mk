4ab50b35e3a513c1f78831c3ffa9e02a0484c137
jignesh2023 <jignesh2023@users.noreply.github.com>
2024-01-06 23:00:28 +0530

    bringing up afterlife

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,7 +7,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_everpal.mk \
     $(LOCAL_DIR)/arrow_everpal.mk \
-    $(LOCAL_DIR)/lineage_everpal.mk
+    $(LOCAL_DIR)/afterlife_everpal.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_everpal-user \
@@ -16,6 +16,6 @@ COMMON_LUNCH_CHOICES := \
     arrow_everpal-user \
     arrow_everpal-userdebug \
     arrow_everpal-eng \
-    lineage_everpal-user \
-    lineage_everpal-userdebug \
-    lineage_everpal-eng
+    afterlife_everpal-user \
+    afterlife_everpal-userdebug \
+    afterlife_everpal-eng
--- afterlife_everpal.mk
+++ afterlife_everpal.mk
@@ -1,15 +1,15 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2019 The afterlifeOS Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifer: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AfterLife stuff.
+$(call inherit-product, vendor/afterlife/config/common.mk)
 
 # Inherit Vendor GApps
 $(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
@@ -17,14 +17,9 @@ $(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
 # Inherit from everpal device
 $(call inherit-product, device/xiaomi/everpal/device.mk)
 
-# LineageOS Stuff
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_AOSP_RECOVERY := true
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_everpal
+PRODUCT_NAME := afterlife_everpal
 PRODUCT_DEVICE := everpal
 PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_BRAND := Redmi
@@ -32,3 +27,21 @@ PRODUCT_MODEL := everpal
 
 # Build info
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Spoof build description/fingerprint as pixel device
+TARGET_USE_PIXEL_FINGERPRINT := true
+
+#GappsFull
+AFTERLIFE_GAPPS := true
+
+#Maintainer
+AFTERLIFE_MAINTAINER := jignesh2023
+
+#Offline Charging
+USE_PIXEL_CHARGING =: true
+
+#Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_GAPPS_ARCH := arm64
+TARGET_USES_AOSP_RECOVERY := true
